9cc7dcb1ebd939ee4575ac9cfde7e1fed8468108
Xaphon <89739217+Xaphon@users.noreply.github.com>
2023-02-26 18:23:56 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/* -- Glow effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 30, fill: "forwards" });
+}
